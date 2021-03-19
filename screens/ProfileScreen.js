79cdc0dd85433f1c11ba4fc99d4d2aa1f002eca5
Sadiyah104 <Sadiyah104@hotmail.com>
2021-03-19 22:23:51 +0000

    change UI, add comments and refactor code

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,6 +1,9 @@
-//             <Button title="settings" onPress={() => navigation.navigate('Settings')} />
+/**
+ * ProfileScreen: Handles displaying the user's details by reading data
+ * from the remote database.
+ */
 import React, { Component } from 'react';
-import { StyleSheet, View, Text, Button, TouchableOpacity, Image, ImageBackground } from 'react-native';
+import { View, Text, Button, TouchableOpacity, Image } from 'react-native';
 import firebase from '../database/firebase';
 import stylesheet from '../styles/stylesheet.js'
 
@@ -12,7 +15,8 @@ export default class ProfileScreen extends Component {
     }
   }
 
-  signOut = () => {
+  // handles signin the user out of their account.
+  onSignOutClick = () => {
     firebase.auth().signOut().then(() => {
       this.props.navigation.navigate('Signin')
     })
@@ -26,28 +30,32 @@ export default class ProfileScreen extends Component {
       uid: firebase.auth().currentUser.uid
     }    
     return (
-        <View>
-        <View style={stylesheet.topContainer}>
-          <Image source={require('../assets/p-trans.png')} style={stylesheet.imageIcon}/>
-        </View>
-      <View style={stylesheet.container}>
-          <View style={stylesheet.inputStyle}>
-            <Text style={stylesheet.text}>Name: {this.state.displayName}</Text>
-          </View>
-          <View style={stylesheet.inputStyle}>
-            <Text style={stylesheet.text}>Email: {this.state.email}</Text>
-          </View>
-          <View style={stylesheet.inputStyle}>
-            <Text style={stylesheet.text}>Password: **********</Text>
-          </View>
-          <Button title="Reset Password" onPress={() => this.props.navigation.navigate('ResetPassword')} />
-        <TouchableOpacity onPress={() => this.signOut()} style={stylesheet.appButtonContainer}>
-            <Text style={ stylesheet.button } onPress={() => this.signOut()}>Logout</Text>
-        </TouchableOpacity>
-      </View>                      
+    <View>
+      {/* Icon */}
+      <View style={stylesheet.topContainer}>
+        <Image source={require('../assets/p-trans.png')} style={stylesheet.imageIcon}/>
       </View>
+
+      {/* Display user data */}
+      <View style={stylesheet.container}>
+        <View style={stylesheet.inputStyle}>
+          <Text style={stylesheet.text}>Name: {this.state.displayName}</Text>
+        </View>
+        <View style={stylesheet.inputStyle}>
+          <Text style={stylesheet.text}>Email: {this.state.email}</Text>
+        </View>
+        <View style={stylesheet.inputStyle}>
+          <Text style={stylesheet.text}>Password: **********</Text>
+        </View>
+
+        {/* Navigation options: reset password + sign out */}
+        <Button title="Reset Password" onPress={() => this.props.navigation.navigate('ResetPassword')} />
+        <TouchableOpacity onPress={() => this.onSignOutClick()} style={stylesheet.appButtonContainer}>
+          <Text style={ stylesheet.button } onPress={() => this.onSignOutClick()}>Logout</Text>
+        </TouchableOpacity>
+        
+      </View>                      
+    </View>
     );
   }
 }
-
-const styles = StyleSheet.create({});
